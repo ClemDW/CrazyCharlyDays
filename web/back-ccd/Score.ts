e176a6489f29c2373de728c3723fefc5b599ee66
Leo Fontaine <leo.fontaine4@orange.fr>
2026-02-19 14:41:54 +0100

    tests score

--- web/back-ccd/Score.ts
+++ web/back-ccd/Score.ts
@@ -1,7 +1,4 @@
-type ScoreResult = {
-    score: number;
-    perBoxScore: Map<string, number>
-}
+import { BoxWithArticle, Campaign, State, UserToChild, ScoreResult } from "./Temp";
 
 const INVALID_SCORE = -9999999999999;
 
