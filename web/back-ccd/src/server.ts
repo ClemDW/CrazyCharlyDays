813ec36308acc128fcb2cd22f3a98e869bc33e42
ClemDW <clementdewasch54@gmail.com>
2026-02-19 21:05:07 +0100

    hotfix: .env

--- web/back-ccd/src/server.ts
+++ web/back-ccd/src/server.ts
@@ -1,3 +1,7 @@
+import * as dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.join(__dirname, "../../../.env") });
+
 import "reflect-metadata";
 import express, { Request, Response } from "express";
 import cors from "cors";
@@ -13,7 +17,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = process.env.BACKEND_PORT;
 
 
 app.get("/articles", ArticleController.getAll);
