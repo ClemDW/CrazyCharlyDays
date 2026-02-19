53c195370e6987a0c1e3849954ecb9e000aee88d
NaasaX <besancon.marcelin@hotmail.com>
2026-02-19 22:33:19 +0100

    hotfix

--- web/back-ccd/init.sql
+++ web/back-ccd/init.sql
@@ -68,6 +68,5 @@ CREATE TABLE Article (
                          price DECIMAL(10, 2),
                          weight FLOAT,
                          id_box UUID REFERENCES Box(id_box),
-                         picture TEXT,
-                         code_barre VARCHAR(100)
+                         picture TEXT
 );
