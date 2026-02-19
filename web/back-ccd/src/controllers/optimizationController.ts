aad17ce09cb28493265aa11c6e5b03e3dff5de43
Leo Fontaine <leo.fontaine4@orange.fr>
2026-02-19 20:48:29 +0100

    changement final optimisation

--- web/back-ccd/src/controllers/optimizationController.ts
+++ web/back-ccd/src/controllers/optimizationController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { saveCSV } from "../csv_manager/csv_saver";
 import Solver from "../optimisation/Solver";
+import {User} from "../entities/User";
 
 export const OptimizationController = {
     /**
@@ -20,10 +21,34 @@ export const OptimizationController = {
             // Sauvegarde le CSV
             await saveCSV(csvOutput, "solution.csv");
 
+            const boxesPromise = result.boxes.map(async (box: any) =>  {
+                const user = await User.findOneBy({ id_user: box.box.id_user });
+                return ({
+                    userName: user ? user.name : "Unknown",
+                    id_user: box.box.id_user,
+                    articles: box.articles.map((a: any) => ({
+                        id: a.id_article,
+                        category: a.category,
+                        age_range: a.age_range,
+                        state: a.state
+                    }))
+                })
+            });
+
+            const boxes = await Promise.all(boxesPromise);
+
+            let json = {
+                csv: csvOutput,
+                jsonObject: {
+                    score: result.score,
+                    boxes: boxes
+                }
+            };
+
             // Retourne le fichier CSV
             res.header("Content-Type", "text/csv");
             res.attachment("solution.csv");
-            return res.send(csvOutput);
+            return res.json(json);
 
         } catch (error) {
             console.error("Erreur lors de l'optimisation :", error);
