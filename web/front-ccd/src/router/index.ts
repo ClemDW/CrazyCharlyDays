f3dec86171cfebf6150e33a9598a9bc3820fd489
NaasaX <besancon.marcelin@hotmail.com>
2026-02-19 13:21:46 +0100

    fix : merge

--- web/front-ccd/src/router/index.ts
+++ web/front-ccd/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import CreateArticle from "../views/CreateArticle.vue";
+import AuthView from "../views/AuthView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,12 +12,14 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      path: "/create-article",
+      name: "create-article",
+      component: CreateArticle,
+    },
+    {
+      path: "/auth",
+      name: "auth",
+      component: AuthView,
     },
   ],
 });
